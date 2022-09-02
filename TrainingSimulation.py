688eb180e094825d77d2bd582862cf2cdec7e96a
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-09-02 17:12:39 +0600

    trained the model

--- TrainingSimulation.py
+++ TrainingSimulation.py
@@ -21,3 +21,20 @@ print('Total Validation Images: ',len(xVal))
 
 model = createModel()
 model.summary()
+
+history = model.fit(batchGen(xTrain, yTrain, 100, 1),
+                                  steps_per_epoch=300,
+                                  epochs=10,
+                                  validation_data=batchGen(xVal, yVal, 100, 0),
+                                  validation_steps=200)
+
+model.save('model.h5')
+print('Model Saved')
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['Training', 'Validation'])
+plt.title('Loss')
+plt.xlabel('Epoch')
+plt.show()
+
