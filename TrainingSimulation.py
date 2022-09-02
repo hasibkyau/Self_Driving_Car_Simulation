f9507ce3407d26b570eaaaf9e2494b78f404abc1
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-09-02 10:03:37 +0600

    modified code

---
+++ TrainingSimulation.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from utlis import *
+
+path = 'myData'
+data = importDataInfo(path)
+# data = pd.DataFrame(data)
+# print(len(data))
+# print(type(data))
+# print(data)
+
+data = balanceData(data, display=False)
+# data = pd.DataFrame(data)
+print(len(data))
+
+imagesPath, steerings = loadData(path,data)
+
+xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steerings, test_size=0.2,random_state=10)
+print('Total Training Images: ',len(xTrain))
+print('Total Validation Images: ',len(xVal))
+
---
+++ utlis.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+
+def getName(filePath):
+    return filePath.split('\\')[-1]
+
+
+def importDataInfo(path):
+    columns = ['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Brake', 'Speed']
+    data = pd.read_csv(os.path.join(path, 'driving_log.csv'), names=columns)
+    #### REMOVE FILE PATH AND GET ONLY FILE NAME
+    # print(getName(data['center'][0]))
+    data['Center'] = data['Center'].apply(getName)
+    # print(data.head())
+    print('Total Images Imported', data.shape[0])
+    return data
+
+
+def balanceData(data,display=True):
+    nBin = 31
+    samplesPerBin = 500
+    hist, bins = np.histogram(data['Steering'], nBin)
+    if display:
+        center = (bins[:-1] + bins[1:]) * 0.5
+        plt.bar(center, hist, width=0.06)
+        plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
+        plt.show()
+    removeindexList = []
+    for j in range(nBin):
+        binDataList = []
+        for i in range(len(data['Steering'])):
+            if data['Steering'][i] >= bins[j] and data['Steering'][i] <= bins[j + 1]:
+                binDataList.append(i)
+        binDataList = shuffle(binDataList)
+        binDataList = binDataList[samplesPerBin:]
+        removeindexList.extend(binDataList)
+
+    print('Removed Images:', len(removeindexList))
+    # print(type(removeindexList))
+    # print(removeindexList)
+    data.drop(data.index[removeindexList], inplace=True)
+    print('Remaining Images:', len(data))
+    # print(type(data))
+
+    if display:
+        hist, _ = np.histogram(data['Steering'], (nBin))
+        plt.bar(center, hist, width=0.06)
+        plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
+        plt.show()
+    return data
+
+
+def loadData(path, data):
+  imagesPath = []
+  steering = []
+  for i in range(len(data)):
+    indexed_data = data.iloc[i]
+    imagesPath.append(f'{path}/IMG/{indexed_data[0]}')
+    steering.append(float(indexed_data[3]))
+  imagesPath = np.asarray(imagesPath)
+  steering = np.asarray(steering)
+  return imagesPath, steering
+
+
