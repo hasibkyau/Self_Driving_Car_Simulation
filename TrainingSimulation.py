47b21d0d85d29d278ec4999a00089d2ef15acb32
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-09-05 22:36:44 +0600

    Spliting off the data

--- TrainingSimulation.py
+++ TrainingSimulation.py
@@ -1,13 +1,18 @@
 from utlis import *
+from sklearn.model_selection import train_test_split
 
-###STEP 01: Initialize Data
+### STEP 01: Initialize Data
 path = 'myData'
 data = importDataInfo(path)
 
-###Step 02: Balance Data
+### Step 02: Balance Data
 data = balanceData(data,display=False)
 
-###Step 03: Preparing for preprocessing
-imagesPath, steering = loadData(path,data)
-print(imagesPath[0], steering[0])
+### Step 03: Preparing for preprocessing
+imagesPath, steerings = loadData(path,data)
+# print(imagesPath[0], steerings[0])
 
+### Step 04: Spliting off the data
+xTrain, xVal, yTrain, yVal = train_test_split(imagesPath,steerings, test_size=0.2,random_state=5)
+print('Total training images: ', len(xTrain))
+print('Total validation images: ', len(xVal))
